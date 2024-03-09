9b03514d1721cff77f2870c14be0ad498acaedc4
dima <dima@kojem.ru>
2024-03-09 14:22:40 +0300

    Initial Commit

---
+++ Lovly Kids/Lovly_KidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lovly_KidsApp.swift
+//  Lovly Kids
+//
+//  Created by Дима Кожемякин on 09.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct Lovly_KidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
