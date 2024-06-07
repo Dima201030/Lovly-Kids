f3c97c52da79f9148c44610a89b3c5921a6ba2e7
dima <115942204+Dima201030@users.noreply.github.com>
2024-06-08 00:04:41 +0300

    Mini update ( secsefuld )

---
+++ Lovly Kids/Core/Messanger/UserProfile/UserProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  UserProfileView.swift
+//  Lovely Kids
+//
+//  Created by Дима Кожемякин on 07.06.2024.
+//
+
+import SwiftUI
+
+struct UserProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserProfileView()
+}
---
+++ Lovly Kids/Core/Messanger/UserProfile/UserProfileViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserProfileViewModel.swift
+//  Lovely Kids
+//
+//  Created by Дима Кожемякин on 07.06.2024.
+//
+
+import Foundation
