5a27266c3490b8f61df0a2b752fd79b86065b0d4
dima <115942204+Dima201030@users.noreply.github.com>
2024-06-12 15:35:01 +0300

    Minor update. Change userProfile

--- Lovly Kids/Core/Messanger/UserProfile/UserProfileView.swift
+++ Lovly Kids/Core/Messanger/UserProfile/UserProfileView.swift
@@ -8,17 +8,167 @@
 import SwiftUI
 
 struct UserProfileView: View {
+    @State private var imageURL: URL?
+    @State private var loadedImage: UIImage?
     @EnvironmentObject var viewModel: UserProfileViewModel
+    @State private var firstNameLetter = ""
+    @State private var sheetShowId = false
     var body: some View {
-        Text("\(viewModel.userFullname)")
-        Text("\(viewModel.userAge)")
-        Text("\(viewModel.userUid)")
-        Text("\(viewModel.userProfileUrl)")
-        Text("\(viewModel.userProfileColor)")
+        ScrollView {
+            VStack{
+                if let loadedImage = loadedImage {
+                    Image(uiImage: loadedImage)
+                        .resizable()
+                        .scaledToFill()
+                        .clipShape(.circle)
+                        .frame(width: 120, height: 120)
+                } else {
+                    Circle()
+                        .frame(width: 120, height: 120)
+                        .foregroundColor(viewModel.userProfileColor)
+                        .overlay(
+                            Text(firstNameLetter)
+                                .font(.system(size: 50, weight: .bold, design: .default))
+                                .foregroundColor(.white)
+                            
+                        )
+                }
+                VStack {
+                    Text("\(viewModel.userFullname)")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    Text("\(viewModel.userEmail)")
+                        .foregroundColor(.gray)
+                }
+                HStack {
+                    Button {
+                        
+                    } label: {
+                        VStack {
+                            Image(systemName: "bell.slash.fill")
+                                .resizable()
+                                .frame(width: 20, height: 20)
+                            Text("Mute")
+                        }
+                        .frame(width: (UIScreen.main.bounds.width / 3) - 10, height: 70)
+                    }
+                    .background(Color(.systemGray5))
+                    .cornerRadius(10)
+                    .foregroundColor(.black)
+                    
+                    Button {
+                        
+                    } label: {
+                        VStack {
+                            Image(systemName: "hand.raised.fill")
+                                .resizable()
+                                .frame(width: 20, height: 25)
+                            Text("Block")
+                        }
+                        .frame(width: (UIScreen.main.bounds.width / 3) - 10, height: 70)
+                    }
+                    .background(Color(.systemGray5))
+                    .cornerRadius(10)
+                    .foregroundColor(.black)
+                    
+                    Button {
+                        sheetShowId.toggle()
+                    } label: {
+                        VStack {
+                            //                                Image(systemName: "eye.fill")
+                            //                                    .resizable()
+                            //                                    .frame(width: 10, height: 10)
+                            Text("Show id")
+                        }
+                        .frame(width: (UIScreen.main.bounds.width / 3) - 10, height: 70)
+                    }
+                    .background(Color(.systemGray5))
+                    .cornerRadius(10)
+                    .foregroundColor(.black)
+                    .sheet(isPresented: $sheetShowId, content: {
+                        Text("\(viewModel.userUid)")
+                        
+                        Button(action: {
+                            // Call the copyToClipboard function and pass textToCopy as an argument to copy the text to the clipboard.
+                            copyToClipboard(text: viewModel.userUid)
+                        }) {
+                            Text("Copy to Clipboard")
+                        }
+                        
+                    })
+                    
+                    //                        VStack {
+                    //                            Image(systemName: "phone")
+                    //                            Text("Phone")
+                    //                        }
+                    //                        .background(Color(.systemGray5))
+                    //                        .cornerRadius(10)
+                    //                        .frame(width: (UIScreen.main.bounds.width / 4) - 20, height: 100)
+                    //                        VStack {
+                    //                            Image(systemName: "phone")
+                    //                            Text("Phone")
+                    //                        }
+                    //                        .background(Color(.systemGray5))
+                    //                        .cornerRadius(10)
+                    //                        .frame(width: (UIScreen.main.bounds.width / 4) - 20, height: 100)
+                    //                        VStack {
+                    //                            Image(systemName: "phone")
+                    //                            Text("Phone")
+                    //                        }
+                    //                        .background(Color(.systemGray5))
+                    //                        .cornerRadius(10)
+                    //                        .frame(width: (UIScreen.main.bounds.width / 4) - 4, height: 100)
+                    //                        VStack {
+                    //                            Image(systemName: "phone")
+                    //                            Text("Phone")
+                    //                        }
+                    //                        .background(Color(.systemGray5))
+                    //                        .cornerRadius(10)
+                    //                        .frame(width: (UIScreen.main.bounds.width / 4) - 20, height: 100)
+                }
+                .padding( 4)
+            }
+            
+            .onAppear() {
+                
+                firstNameLetter = String(viewModel.userFullname.prefix(1))
+                
+                
+                imageURL = URL(string: viewModel.userProfileUrl)
+                
+                if let imageURL {
+                    image(from: imageURL) { image in
+                        loadedImage = image
+                    }
+                }
+        }
+        }
+    }
+    //        Text("\(viewModel.userFullname)")
+    //        Text("\(viewModel.userAge)")
+    //        Text("\(viewModel.userUid)")
+    //        Text("\(viewModel.userProfileUrl)")
+    //        Text("\(viewModel.userProfileColor)")
+    
+    func image(from url: URL, completion: @escaping (UIImage?) -> Void) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data, let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    completion(image)
+                }
+            } else {
+                DispatchQueue.main.async {
+                    completion(nil)
+                }
+            }
+        }
+        .resume()
+    }
+    func copyToClipboard(text: String) {
+        UIPasteboard.general.string = text
     }
 }
 
 #Preview {
     UserProfileView()
-        .environmentObject(UserProfileViewModel())
 }
--- Lovly Kids/Core/Messanger/UserProfile/UserProfileViewModel.swift
+++ Lovly Kids/Core/Messanger/UserProfile/UserProfileViewModel.swift
@@ -8,10 +8,11 @@
 import SwiftUI
 
 class UserProfileViewModel: ObservableObject {
-    @Published var userFullname = "Дмитрий"
+    @Published var userFullname = "Nevsk1y"
     @Published var userAge = 15
     @Published var userUid = "вфлоиазловипщжцL"
     @Published var userProfileUrl = "https://firebasestorage.googleapis.com:443/v0/b/messanger-8c488.appspot.com/o/images%2F530FB321-D960-49E2-A5D8-2887F31D47D1.jpg?alt=media&token=c3e12585-4410-490b-becd-2efee0446d4b"
     @Published var userProfileColor = Color.blue
+    @Published var userEmail = "Dm.koj.itrii@gmail.com"
     
 }
